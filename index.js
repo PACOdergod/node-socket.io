0214008e844bf2aebd22110185c07e68650762a0
paco <paco.33353@gmail.com>
2021-06-02 21:54:58 -0500

    server terminado

--- index.js
+++ index.js
@@ -5,21 +5,60 @@ require('dotenv').config()
 const app = express()
 
 
+// bandas
+const Bands = require('./models/bands')
+const Band = require('./models/band')
+const bands = new Bands()
+
+bands.addBand(new Band('Queen'))
+bands.addBand(new Band('Bon Jovi'))
+bands.addBand(new Band('Panda'))
+bands.addBand(new Band('Poison'))
+
+// console.log(bands);
+
 // node Server
 const server = require('http').createServer(app);
 const io = require('socket.io')(server)
 
 io.on('connection', client => {
     console.log('cliente conectado');
-    // client.on('event', data => { /* … */ });
+
+    //emito solo al que se esta conectando
+    client.emit('active-bands', bands.getbands())
+
     client.on('disconnect', () => { 
         console.log('cliente desconectado');
     });
 
-    client.on('mensaje', ( payload )=>{
-        console.log('server recibio ', payload);
+    client.on('votar', ( payload )=>{
+        // console.log( payload );
 
-        io.emit('mensaje', {server: 'nuevo nombre'})
+        bands.voteBand( payload.id )
+        // notificar a todos los dispositivos 
+        // conectados al servidor
+        client.emit('active-bands', bands.getbands())
+    })
+
+    // escuchar nueva banda
+    client.on('new-band', ( payload )=>{
+        console.log(payload);
+        bands.addBand(new Band(payload.nombre))
+        client.emit('active-bands', bands.getbands())
+    })
+
+    // eliminar banda
+    client.on('delete-band', ( payload )=>{
+        console.log(payload);
+        bands.deleteBand(payload.id)
+        client.emit('active-bands', bands.getbands())
+    })
+
+
+    client.on('nuevo-mensaje', ( payload )=>{
+        // emite a todos menos al que envio el ensaje
+        client.broadcast.emit('nuevo-mensaje', payload)
+        console.log(payload);
     })
 });
 
